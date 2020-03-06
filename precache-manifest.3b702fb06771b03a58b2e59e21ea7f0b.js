6691741cfd28fc40115b9c88f0dfc04a15590583
Asma Srour <asma.srour96@gmail.com>
2020-03-07 01:17:57 +0200

    Updates

---
+++ precache-manifest.3b702fb06771b03a58b2e59e21ea7f0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db249e70b1ad648525d8103300cbba20",
+    "url": "/NotePad/index.html"
+  },
+  {
+    "revision": "5f00935bec3be968e5a3",
+    "url": "/NotePad/static/css/2.51f4a81e.chunk.css"
+  },
+  {
+    "revision": "759a7c87bf52f58bc703",
+    "url": "/NotePad/static/css/main.672a9944.chunk.css"
+  },
+  {
+    "revision": "5f00935bec3be968e5a3",
+    "url": "/NotePad/static/js/2.66d3a272.chunk.js"
+  },
+  {
+    "revision": "0d51a26e357591260d29df3c0b1559d1",
+    "url": "/NotePad/static/js/2.66d3a272.chunk.js.LICENSE"
+  },
+  {
+    "revision": "759a7c87bf52f58bc703",
+    "url": "/NotePad/static/js/main.7d2d69c7.chunk.js"
+  },
+  {
+    "revision": "cb9159ca05d9d7df9778",
+    "url": "/NotePad/static/js/runtime-main.47d5b9b8.js"
+  },
+  {
+    "revision": "a130bad92dc800cfa5541cd88b6f8683",
+    "url": "/NotePad/static/media/background.a130bad9.jpg"
+  },
+  {
+    "revision": "2a2280172022d3d044fc26f36bfe16f9",
+    "url": "/NotePad/static/media/notes.2a228017.ico"
+  }
+]);
